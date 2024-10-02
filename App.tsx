275ff61e403d14d44b27e4bfd14c28e64e2f7036
ST10204902 <ST10204902@vcconnect.edu.za>
2024-10-02 11:51:36 +0200

    Added auth and firebaseConfig changes

--- App.tsx
+++ App.tsx
@@ -1,5 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import './src/firebase/firebaseConfig.js'; //Firebase config file
+
 
 export default function App() {
   return (
