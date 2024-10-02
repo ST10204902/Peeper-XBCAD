3876b4648a956227dcacc6e6c4061c616ef5f1b4
ST10204902 <ST10204902@vcconnect.edu.za>
2024-10-02 15:56:24 +0200

    Working on register

--- src/screens/RegisterScreen.tsx
+++ src/screens/RegisterScreen.tsx
@@ -1,12 +1,33 @@
 // App.tsx
-import React from "react";
+import * as React from "react";
 import { SafeAreaView, Alert } from "react-native";
+import { Text, TextInput, TouchableOpacity, View } from "react-native";
 import CustomButton from "../components/LoginRegisterButtonComponent"; // Ensure the path is correct
+import { useSignUp } from "@clerk/clerk-expo";
+import { SharedTransitionType } from "react-native-reanimated";
+import { RootStackParamsList } from "./RootStackParamsList";
 
-const RegisterScreen: React.FC = () => {
-  const handlePress = () => {
-    Alert.alert("Button Pressed!", "You have pressed the button.");
-  };
+export default function RegisterScreen() {
+  const {isLoaded, signUp} = useSignUp();
+  const [emailAddress, setEmailAddress] = React.useState("");
+
+  const onSignUpPress = async () => {
+    if(!isLoaded) {
+      return;
+    }
+
+    try{
+      await signUp.create({emailAddress: emailAddress});
+
+      await signUp.prepareEmailAddressVerification({strategy: "email_code"});
+
+      navigator.
+    }
+
+    
+
+
+  }
 
   return (
     <SafeAreaView
@@ -16,5 +37,3 @@ const RegisterScreen: React.FC = () => {
     </SafeAreaView>
   );
 };
-
-export default RegisterScreen;
