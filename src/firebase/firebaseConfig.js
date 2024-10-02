c9c829f60d70189158a2411374758681fd2ea713
ST10204902 <ST10204902@vcconnect.edu.za>
2024-10-02 11:33:59 +0200

    Add firebase

---
+++ src/firebase/firebaseConfig.js
@@ -0,0 +1,24 @@
+
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getDatabase } from "firebase/database";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyC79dLUv5wmpvYmreVMDS0KMFcmw-RUEMk",
+  authDomain: "peeper-xbcad.firebaseapp.com",
+  projectId: "peeper-xbcad",
+  storageBucket: "peeper-xbcad.appspot.com",
+  messagingSenderId: "761357003841",
+  appId: "1:761357003841:web:97de31e23cc0c0f449aac0",
+  measurementId: "G-8K27P2SQER"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const database = getDatabase(app);
