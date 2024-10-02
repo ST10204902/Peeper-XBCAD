c6208ea2899509722ed377992323d0709528f2ab
Anneme <ST10144453@vcconnect.edu.za>
2024-10-02 15:36:06 +0200

    login and register views + hyperlink

--- src/screens/RegisterScreen.tsx
+++ src/screens/RegisterScreen.tsx
@@ -1,26 +1,32 @@
-// App.tsx
+// RegisterScreen.tsx
+//----------Imports---------//
 import React from "react";
-import { SafeAreaView, Alert } from "react-native";
+import { SafeAreaView, Alert, View } from "react-native";
 import CustomButton from "../components/LoginRegisterButtonComponent"; // Ensure the path is correct
 import styles from "../styles/RegisterScreenStyle"; // Ensure the path is correct
+import LoginRegisterHyperlink from "../components/LoginRegisterHyperlink"; // Ensure the path is correct
 
+//----------Code---------//
+//Demonstrating where to implement pressing the button.
 const RegisterScreen: React.FC = () => {
-  
   const handlePress = () => {
-    Alert.alert("Button Pressed!", "You have pressed the button.");
     Alert.alert("Button Pressed!", "You have pressed the button.");
   };
 
+  //Returns the components for the screen
   return (
     <SafeAreaView style={styles.container}>
       <CustomButton
         onPress={handlePress}
-        title="YOUR MOM"
+        title="Register"
         textColor="#334FD7"
         buttonColor="#ffffff"
         fontFamily="Rany-Medium"
         textSize={30}
       />
+      <LoginRegisterHyperlink toLogin={true}>
+        Already have an account? Log in
+      </LoginRegisterHyperlink>
     </SafeAreaView>
   );
 };
---
+++ src/styles/LoginRegisterHyperlinkStyle.tsx
@@ -0,0 +1,13 @@
+// src/styles/LoginRegisterButtonStyles.js
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+  hyperlinkText: {
+    fontSize:20,
+    textAlign: 'center',
+    color: '#FFFFFF',
+    backgroundColor: 'transparent',
+    textDecorationLine: 'underline',
+    marginTop: 20,
+  },
+});
