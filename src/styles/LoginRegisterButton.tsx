29bddcce0c8e7e5ec65fb6f15f909f90eb65ef83
Anneme <ST10144453@vcconnect.edu.za>
2024-10-02 11:46:03 +0200

    Added first iteration of button component

---
+++ src/styles/LoginRegisterButton.tsx
@@ -0,0 +1,18 @@
+// src/styles/LoginRegisterButtonStyles.js
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+  button: {
+    backgroundColor: '#1E90FF',
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderRadius: 5,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginVertical: 10,
+  },
+  buttonText: {
+    color: '#fff',
+    fontSize: 16,
+  },
+});
