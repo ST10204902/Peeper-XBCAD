59c79353bac97829b8fb3f7be6cfd9e252a4c960
Anneme <ST10144453@vcconnect.edu.za>
2024-10-02 14:43:30 +0200

    add button

--- src/components/LoginRegisterButtonComponent.tsx
+++ src/components/LoginRegisterButtonComponent.tsx
@@ -1,17 +1,40 @@
-// src/components/LoginRegisterButtonComponent.tsx
-import React from 'react';
-import { TouchableOpacity, Text, GestureResponderEvent } from 'react-native';
-import styles from '../styles/LoginRegisterButton'; // Adjust path if needed
+import React, { useState } from 'react';
+import { TouchableOpacity, Text, GestureResponderEvent, StyleSheet, ViewStyle, TextStyle } from 'react-native';
+import styles from '../styles/LoginRegisterButton'; // Ensure the path is correct
 
 interface CustomButtonProps {
   onPress: (event: GestureResponderEvent) => void;
   title: string;
+  textColor?: string; // Add an optional prop for text color
+  buttonColor?: string; // Add an optional prop for button color
+  fontFamily?: string; // Add an optional prop for font family
+  textSize?: number; // Add an optional prop for text size
 }
 
-const CustomButton: React.FC<CustomButtonProps> = ({ onPress, title }) => {
+const CustomButton: React.FC<CustomButtonProps> = ({ onPress, title, textColor, buttonColor, fontFamily, textSize }) => {
+  const [buttonHeight, setButtonHeight] = useState<number>(0);
+
+  const handleLayout = (event: any) => {
+    const { height } = event.nativeEvent.layout;
+    setButtonHeight(height);
+  };
+
+  const dynamicButtonStyle: ViewStyle = {
+    backgroundColor: buttonColor || '#fff',
+    borderRadius: buttonHeight / 3,
+  };
+
+  const dynamicTextStyle: TextStyle = {
+    color: textColor || '#334FD7',
+    fontFamily: fontFamily || 'System',
+    fontSize: textSize || 25,
+  };
+
   return (
-    <TouchableOpacity style={styles.button} onPress={onPress}>
-      <Text style={styles.buttonText}>{title}</Text>
+    <TouchableOpacity style={[styles.button, dynamicButtonStyle]} onLayout={handleLayout} onPress={onPress}>
+      <Text style={[styles.buttonText, dynamicTextStyle]}>
+        {title}
+      </Text>
     </TouchableOpacity>
   );
 };
