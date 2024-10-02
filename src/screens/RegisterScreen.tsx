25f4ef42a92213a333397e612dd11118bd7838cf
Anneme <ST10144453@vcconnect.edu.za>
2024-10-02 14:43:30 +0200

    add button

--- src/screens/RegisterScreen.tsx
+++ src/screens/RegisterScreen.tsx
@@ -2,19 +2,27 @@
 import React from "react";
 import { SafeAreaView, Alert } from "react-native";
 import CustomButton from "../components/LoginRegisterButtonComponent"; // Ensure the path is correct
+import styles from "../styles/RegisterScreenStyle"; // Ensure the path is correct
 
-const RegisterScreen: React.FC = () => {
+const App: React.FC = () => {
+  
   const handlePress = () => {
+    Alert.alert("Button Pressed!", "You have pressed the button.");
     Alert.alert("Button Pressed!", "You have pressed the button.");
   };
 
   return (
-    <SafeAreaView
-      style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
-    >
-      <CustomButton onPress={handlePress} title="Register" />
+    <SafeAreaView style={styles.container}>
+      <CustomButton
+        onPress={handlePress}
+        title="YOUR MOM"
+        textColor="#334FD7"
+        buttonColor="#ffffff"
+        fontFamily="Rany-Medium"
+        textSize={30}
+      />
     </SafeAreaView>
   );
 };
 
-export default RegisterScreen;
+export default App;
