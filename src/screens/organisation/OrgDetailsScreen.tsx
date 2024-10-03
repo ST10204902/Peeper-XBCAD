0a663970067b2b621829c459053448fd7f9efd85
Try-Angle1134 <Jaspervann1313@gmail.com>
2024-10-03 16:45:03 +0200

    Refactor code structure and add new components
    - Refactor code structure to improve organization and readability
    - Add new components: StudentHeaderComponent and CustomButton
    - Update OrgDetailsScreen to include new components and student information
    - Add EmergencyContacts component for displaying emergency contact information
    
    Related work items: #123, #456

--- src/screens/organisation/OrgDetailsScreen.tsx
+++ src/screens/organisation/OrgDetailsScreen.tsx
@@ -1,8 +1,22 @@
 //import { Text } from "react-native";
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
-import ManageOrgsScreen from "./ManageOrgsScreen";
+import StudentHeaderComponent from "../../components/StudentHeaderComponent";
+import CustomButton from "../../components/CustomButton";
+import EmergencyContacts from "../../components/EmergencyContacts";
 
-export default function OrgDetailsScreen() {
-  return <ManageOrgsScreen></ManageOrgsScreen>;
+const OrgDetailsScreen= () => {
+  const currentStudent = null//set student here
+
+  return (
+    <View>
+      <StudentHeaderComponent currentStudent={currentStudent}/>
+      <CustomButton onPress={() => {}} title="Organisation Management" textColor="black" buttonColor="#A4DB51" fontFamily="Quittance" textSize={20}/>
+      //Add student history map here
+      <text>Be Safe!</text>
+      <EmergencyContacts/>
+    </View>
+  );
 }
+
+export default  OrgDetailsScreen;
