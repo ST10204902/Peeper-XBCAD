7e792cfbbf783fe8c5aa0cdd9ba7b225f57d37c3
Anneme <ST10144453@vcconnect.edu.za>
2024-10-03 10:48:58 +0200

    make login and register done

--- src/styles/RegisterScreenStyle.tsx
+++ src/styles/RegisterScreenStyle.tsx
@@ -5,9 +5,25 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
-    backgroundColor: "#334FD7", // Electric blue background colour 
+    backgroundColor: "#334FD7", // Monster green background colour 
+    paddingHorizontal: 20,
   },
-  
+  headingContainer: {
+    alignSelf: 'flex-start', // Align to the left
+    marginBottom: 15, // Add space below the heading
+    marginTop: 90, // Add space above the heading
+  },
+  inputContainer: {
+    width: '100%', // Ensure the input takes the full width
+    marginBottom: 40, // Add space below the input
+  },
+  buttonContainer: {
+    width: '80%', // Ensure the button takes the full width
+    marginBottom: 0, // Add space below the button
+  },
+  hyperlinkContainer: {
+    width: '100%', // Ensure the hyperlink takes the full width
+  },
 });
 
 export default styles;
