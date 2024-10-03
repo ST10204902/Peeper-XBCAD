f411cf5e7a227e43f40703c8c43fcf9971d202e2
ST10180919 <st10180919@vcconnect.edu.za>
2024-10-03 11:36:52 +0200

    Improved the Expandable orgs list and it's item
    - Moved the button component and function called to the props for the ExpandableOrgsList to allow for greater customisability.
    - Added comments

--- src/screens/organisation/OrgDetailsScreen.tsx
+++ src/screens/organisation/OrgDetailsScreen.tsx
@@ -1,60 +1,8 @@
 //import { Text } from "react-native";
 import React from "react";
-import ExpandableOrgList from "../../components/ExpandableOrgList";
-import { OrgAddress } from "../../models/OrgAddress";
-import { Organisation } from "../../models/Organisation";
-import { StyleSheet, View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
+import ManageOrgsScreen from "./ManageOrgsScreen";
 
 export default function OrgDetailsScreen() {
-  //return <Text> OrgDetailsScreen </Text>;
-  const address: OrgAddress = {
-    streetAddress: "53 Main Rd",
-    suburb: "Claremont",
-    city: "Cape Town",
-    province: "Western Cape",
-    postalCode: "7700",
-  };
-
-  const itemList: Organisation[] = [
-    {
-      org_id: "1",
-      orgName: "Massage The Pandas",
-      orgAddress: address,
-      orgEmail: "MassageThePandas@gmail.com",
-      orgPhoneNo: "1999999999",
-      orgLatitude: 0,
-      orgLongitude: 0,
-    },
-    {
-      org_id: "2",
-      orgName: "Homeless People Shouldn't be Homeless",
-      orgAddress: address,
-      orgEmail: "HomelessPeopleBad@gmail.com",
-      orgPhoneNo: "1999999999",
-      orgLatitude: 0,
-      orgLongitude: 0,
-    },
-    {
-      org_id: "3",
-      orgName: "Starving Women Need Makeup (SWNM)",
-      orgAddress: address,
-      orgEmail: "StarvingWomenNeedMakeup@gmail.com",
-      orgPhoneNo: "1999999999",
-      orgLatitude: 0,
-      orgLongitude: 0,
-    },
-  ];
-  return (
-    <View style={styles.page}>
-      <ExpandableOrgList items={itemList} />
-    </View>
-  );
+  return <ManageOrgsScreen></ManageOrgsScreen>;
 }
-
-const styles = StyleSheet.create({
-  // Add styles here
-  page: {
-    backgroundColor: "#FFFFFF",
-    flex: 1,
-  },
-});
