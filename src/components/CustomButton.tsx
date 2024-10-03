5c5f6fcdbbb4eeffbc2a405f18d622ce2df34cf4
ST10180919 <st10180919@vcconnect.edu.za>
2024-10-03 11:36:52 +0200

    Improved the Expandable orgs list and it's item
    - Moved the button component and function called to the props for the ExpandableOrgsList to allow for greater customisability.
    - Added comments

--- src/components/CustomButton.tsx
+++ src/components/CustomButton.tsx
@@ -1,5 +1,13 @@
-import React, { useState } from 'react';
-import { TouchableOpacity, Text, GestureResponderEvent, View, ViewStyle, TextStyle, StyleSheet } from 'react-native';
+import React, { useState } from "react";
+import {
+  TouchableOpacity,
+  Text,
+  GestureResponderEvent,
+  View,
+  ViewStyle,
+  TextStyle,
+  StyleSheet,
+} from "react-native";
 
 interface CustomButtonProps {
   onPress: (event: GestureResponderEvent) => void;
@@ -11,7 +19,15 @@ interface CustomButtonProps {
   label?: string; // Add an optional prop for the label above the button
 }
 
-const CustomButton: React.FC<CustomButtonProps> = ({ onPress, title, textColor, buttonColor, fontFamily, textSize, label }) => {
+const CustomButton: React.FC<CustomButtonProps> = ({
+  onPress,
+  title,
+  textColor,
+  buttonColor,
+  fontFamily,
+  textSize,
+  label,
+}) => {
   const [buttonHeight, setButtonHeight] = useState<number>(0);
 
   const handleLayout = (event: any) => {
@@ -20,29 +36,31 @@ const CustomButton: React.FC<CustomButtonProps> = ({ onPress, title, textColor, 
   };
 
   const dynamicButtonStyle: ViewStyle = {
-    backgroundColor: buttonColor || '#fff',
+    backgroundColor: buttonColor || "#fff",
     borderRadius: buttonHeight / 3,
   };
 
   const dynamicTextStyle: TextStyle = {
-    color: textColor || '#334FD7',
-    fontFamily: fontFamily || 'Rany-Medium',
+    color: textColor || "#334FD7",
+    fontFamily: fontFamily || "Rany-Medium",
     fontSize: textSize || 25,
   };
 
   const labelTextStyle: TextStyle = {
     marginBottom: 5, // Add some spacing between the label and the button
     fontSize: textSize || 20, // Optional: Match label text size with button text size
-    fontFamily: fontFamily || 'Rany-Medium',
+    fontFamily: fontFamily || "Rany-Medium",
   };
 
   return (
     <View style={localStyles.container}>
       {label && <Text style={labelTextStyle}>{label}</Text>}
-      <TouchableOpacity style={[localStyles.button, dynamicButtonStyle]} onLayout={handleLayout} onPress={onPress}>
-        <Text style={[dynamicTextStyle]}>
-          {title}
-        </Text>
+      <TouchableOpacity
+        style={[localStyles.button, dynamicButtonStyle]}
+        onLayout={handleLayout}
+        onPress={onPress}
+      >
+        <Text style={[dynamicTextStyle]}>{title}</Text>
       </TouchableOpacity>
     </View>
   );
@@ -51,14 +69,16 @@ const CustomButton: React.FC<CustomButtonProps> = ({ onPress, title, textColor, 
 const localStyles = StyleSheet.create({
   container: {
     flexDirection: "row",
+    flexDirection: "row",
   },
   button: {
+    flex: 1,
     flex: 1,
     paddingVertical: 15,
     paddingHorizontal: 20,
     alignItems: "center",
     justifyContent: "center",
-  },
+  },
 });
 
 export default CustomButton;
--- src/components/ExpandableOrgList.tsx
+++ src/components/ExpandableOrgList.tsx
@@ -1,12 +1,34 @@
 import { View, FlatList, StyleSheet } from "react-native";
 import { Organisation } from "../models/Organisation";
 import ExpandableOrgListItem from "./ExpandableOrgListItem";
+import React from "react";
 
+/**
+ * Defines the properties to be passed to the expandable Orgs List
+ * @items List of organisations
+ * @listButtonComp Component for the button contained in the expanded
+ * version of the list item
+ * @onListButtonClicked Function run when the listButtonComp is clicked
+ */
 interface Props {
   items: Organisation[];
+  listButtonComp: React.ReactNode;
+  onListButtonClicked: (selectedOrg: Organisation) => void;
 }
 
-export default function ExpandableOrgList({ items }: Props) {
+/**
+ * Function creating an ExpandableOrgList component
+ * @param items List of organisations
+ * @param listButtonComp Component for the button contained in the expanded
+ * version of the list item
+ * @param onListButtonClicked Function run when the listButtonComp is clicked
+ * @returns The ExpandableOrgList component
+ */
+export default function ExpandableOrgList({
+  items,
+  listButtonComp,
+  onListButtonClicked,
+}: Props) {
   return (
     <View style={styles.listContainer}>
       <FlatList
@@ -16,6 +38,10 @@ export default function ExpandableOrgList({ items }: Props) {
             orgName={item.orgName}
             orgAddress={item.orgAddress}
             oddOrEven={index % 2 === 0 ? "even" : "odd"}
+            listButton={React.cloneElement(
+              listButtonComp as React.ReactElement,
+              { onPress: () => onListButtonClicked(item) }
+            )}
           />
         )}
       />
