8cd9a8eaa89303cdebbb67b4eda5a0cfb1373ccc
ST10180919 <st10180919@vcconnect.edu.za>
2024-10-17 18:21:40 +0200

    Added navigation to the RegisterProfilePhotoScreen
    -Removed redundant navigation upon the changing of  isSignedIn state from Clerke from the LandingScreen, LoginScreen, RegisterScreen, and AppNavigator in App.tsx.
    
    - Changed the RegisterScreen to navigate to the RegisterProfilePhotoScreen, passing the newStudent to set the profile photo chosen.

--- src/screens/RegisterProfilePhotoScreen.tsx
+++ src/screens/RegisterProfilePhotoScreen.tsx
@@ -1,22 +1,56 @@
-import { Text, View } from "react-native";
+import { Alert, Text, View } from "react-native";
 import AvatarComponent from "../components/AvatarComponent";
 import CustomButton from "../components/CustomButton";
 import { useState } from "react";
+import {
+  NavigationProp,
+  RouteProp,
+  useNavigation,
+  useRoute,
+} from "@react-navigation/native";
+import { RootStackParamsList } from "./RootStackParamsList";
 
-function SubmitButtonEvent() {
-  //Do submit button logic here
-  console.log("Submit Button Pressed");
-}
+type RegisterProfilePhotoScreenRouteProp = RouteProp<
+  RootStackParamsList,
+  "RegisterProfilePhotoScreen"
+>;
 
-const RegisterProfilePhotoScreen = () => {
+function RegisterProfilePhotoScreen() {
   const [avatarURI, setAvatarURI] = useState("");
+
+  // Getting student from navigation
+  const route = useRoute<RegisterProfilePhotoScreenRouteProp>();
+  const navigation =
+    useNavigation<NavigationProp<RootStackParamsList, "BottomNavigationBar">>();
+
+  // Access newStudent from the navigation route params
+  const newStudent = route.params;
+
+  const handleSubmit = async () => {
+    // Save profile photo logic here
+    try {
+      await newStudent.save();
+      Alert.alert(newStudent.email);
+      // Navigate to BottomNavigationBar after success
+      navigation.navigate("BottomNavigationBar");
+    } catch (error) {
+      console.error(`Error saving new student: `, error);
+    }
+  };
 
   return (
     <View>
       <Text>Almost Done!</Text>
       <Text>Choose Avatar:</Text>
       <AvatarComponent onAvatarSelected={(uri: string) => setAvatarURI(uri)} />
-      <CustomButton onPress={SubmitButtonEvent} title="SUBMIT" textColor="black" buttonColor="#EC4E4B" fontFamily="Quittance" textSize={20}/>
+      <CustomButton
+        onPress={handleSubmit}
+        title="SUBMIT"
+        textColor="black"
+        buttonColor="#EC4E4B"
+        fontFamily="Quittance"
+        textSize={20}
+      />
     </View>
   );
 }
