cc9af2e30704a546d7d1f089d7ea188f4c8ef467
ST10204902 <ST10204902@vcconnect.edu.za>
2024-10-17 15:15:07 +0200

    Added PDFShareComponent  to SettingsScreen

--- src/screens/settings/SettingsScreen.tsx
+++ src/screens/settings/SettingsScreen.tsx
@@ -1,10 +1,16 @@
 import { Text } from "react-native";
+import PDFShareComponent from "../../components/PDFShareComponent";
 
 /**
  * Screen Component where the user can change their settings
  * @returns a created SettingsScreen Component
  */
 export default function SettingsScreen() {
-  return <Text> SettingsScreen </Text>;
+  return (
+    <>
+      <Text> SettingsScreen </Text>
+      <PDFShareComponent />
+    </>
+  );
 }
 // End of File
