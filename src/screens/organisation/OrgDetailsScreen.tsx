3d9d53bfa4ca80d888d4a1800ff39eb4785d1312
moldybaguette <45487141+moldybaguette@users.noreply.github.com>
2024-10-18 10:47:58 +0200

    cdd

--- src/screens/organisation/OrgDetailsScreen.tsx
+++ src/screens/organisation/OrgDetailsScreen.tsx
@@ -1,11 +1,12 @@
 //import { Text } from "react-native";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { SafeAreaView, StyleSheet, Text, View } from "react-native";
 import StudentHeaderComponent from "../../components/StudentheaderComponent";
 import CustomButton from "../../components/CustomButton";
 import EmergencyContacts from "../../components/EmergencyContacts";
 import { useUser } from "@clerk/clerk-expo";
 import { Student } from "../../databaseModels/databaseClasses/Student";
+import MapSessionHistory from "../../components/MapSessionHistory";
 
 const OrgDetailsScreen = () => {
   const clerkUser = useUser();
@@ -22,13 +23,12 @@ const OrgDetailsScreen = () => {
     fetchStudent();
   }, [clerkUser.user?.id]);
 
-
   return (
-    <View>
+    <SafeAreaView style={styles.container}>
       {loading ? (
         <Text>Loading...</Text>
       ) : (
-        currentStudent && <StudentHeaderComponent currentStudent={currentStudent}/> 
+        currentStudent && <StudentHeaderComponent currentStudent={currentStudent} />
       )}
       <CustomButton
         onPress={() => {}}
@@ -38,9 +38,10 @@ const OrgDetailsScreen = () => {
         fontFamily="Quittance"
         textSize={20}
       />
+      <MapSessionHistory />
       <Text style={styles.BeSafeText}>Be Safe!</Text>
       <EmergencyContacts />
-    </View>
+    </SafeAreaView>
   );
 };
 const styles = StyleSheet.create({
@@ -49,8 +50,33 @@ const styles = StyleSheet.create({
     fontSize: 20,
     textAlign: "left",
     margin: 10,
-  }
+  },
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+  },
+  sheetHeader: {
+    padding: 16,
+    alignItems: "center",
+    backgroundColor: "#f8f8f8",
+  },
+  sheetHeading: {
+    fontSize: 24,
+    fontWeight: "bold",
+    color: "#161616",
+    marginBottom: 10,
+  },
+  listContentContainer: {
+    backgroundColor: "#fff",
+  },
+  itemContainer: {
+    paddingVertical: 10,
+    borderBottomWidth: 1,
+    borderBottomColor: "#ccc",
+  },
+  itemText: {
+    fontSize: 16,
+  },
 });
-
 
 export default OrgDetailsScreen;
