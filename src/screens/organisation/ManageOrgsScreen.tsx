85b55a4e91884c7d629e22f8dff402a526a510a1
ST10195824 <ST10195824@vcconnect.edu.za>
2024-10-23 19:08:09 +0200

    sorting now works

--- src/screens/organisation/ManageOrgsScreen.tsx
+++ src/screens/organisation/ManageOrgsScreen.tsx
@@ -22,24 +22,24 @@ import { useCurrentStudent } from "../../hooks/useCurrentStudent";
  */
 /**
  * ManageOrgsScreen component renders a screen for managing organizations.
- * 
+ *
  * This component displays a list of organizations with options to add, edit, and remove organizations.
  * It also includes a search bar and a combo box for sorting the organizations.
- * 
+ *
  * @component
  * @returns {JSX.Element} The rendered component.
- * 
+ *
  * @example
  * // Usage example:
  * <ManageOrgsScreen />
- * 
+ *
  * @typedef {Object} OrgAddressData
  * @property {string} streetAddress - The street address of the organization.
  * @property {string} suburb - The suburb of the organization.
  * @property {string} city - The city of the organization.
  * @property {string} province - The province of the organization.
  * @property {string} postalCode - The postal code of the organization.
- * 
+ *
  * @typedef {Object} OrganisationData
  * @property {string} org_id - The unique identifier of the organization.
  * @property {string} orgName - The name of the organization.
@@ -52,23 +52,19 @@ import { useCurrentStudent } from "../../hooks/useCurrentStudent";
 export default function ManageOrgsScreen() {
   const navigation = useNavigation();
   const clerkUser = useUser();
-  const {
-    currentStudent,
-    error,
-    loading,
-    saving,
-    updateCurrentStudent,
-  } = useCurrentStudent();
+  const { currentStudent, error, loading, saving, updateCurrentStudent } = useCurrentStudent();
   const [allOrganisations, setAllOrganisations] = React.useState<OrganisationData[]>([]);
-  const [displayedOrganisations, setDisplayedOrganisations] = React.useState<OrganisationData[]>([]);
+  const [displayedOrganisations, setDisplayedOrganisations] = React.useState<OrganisationData[]>(
+    []
+  );
   const [studentOrganisations, setStudentOrganisations] = React.useState<OrganisationData[]>([]);
   const [studentsOrgsLoaded, setStudentsOrgsLoaded] = React.useState<boolean>(false);
+  const [sortBy, setSortBy] = React.useState<string>("name_asc");
   const allOrgs = Organisation.getAllOrganisations();
   let itemList: OrganisationData[] = [];
-  
 
-   // Method to fetch data
-   const fetchData = async () => {
+  // Method to fetch data
+  const fetchData = async () => {
     if (!clerkUser.user) {
       console.error("Clerk user not found in ManageOrgsScreen");
       return;
@@ -76,11 +72,19 @@ export default function ManageOrgsScreen() {
     // Fetch all organisations and student's organisations
     const allOrgs = await Organisation.getAllOrganisations();
     const studentOrgs = await Organisation.getStudentsOrgs(currentStudent?.activeOrgs ?? []);
-    
+
     // Set the state variables with the fetched data
-    setAllOrganisations(allOrgs.filter(org => org && typeof org.toJSON === 'function').map((org) => org.toJSON()));
-    setDisplayedOrganisations(allOrgs.filter(org => org && typeof org.toJSON === 'function').map((org) => org.toJSON()));
-    setStudentOrganisations(studentOrgs.filter(org => org && typeof org.toJSON === 'function').map((org) => org.toJSON()));
+    setAllOrganisations(
+      allOrgs.filter((org) => org && typeof org.toJSON === "function").map((org) => org.toJSON())
+    );
+    setDisplayedOrganisations(
+      allOrgs.filter((org) => org && typeof org.toJSON === "function").map((org) => org.toJSON()).sort((a, b) => a.orgName.localeCompare(b.orgName))
+    );
+    setStudentOrganisations(
+      studentOrgs
+        .filter((org) => org && typeof org.toJSON === "function")
+        .map((org) => org.toJSON())
+    );
     setStudentsOrgsLoaded(true);
   };
 
@@ -91,15 +95,13 @@ export default function ManageOrgsScreen() {
     }
   }, [currentStudent]);
 
-
-  
   /**
    * This function activates when the user clicks on the button for a given
    * expandable orgList
    * @param org Corresponding organisation
    */
   function onStudentOrgsListButtonPressed(org: OrganisationData) {
-   // start tracking pop up
+    // start tracking pop up
   }
 
   /**
@@ -108,69 +110,123 @@ export default function ManageOrgsScreen() {
    * @param org Corresponding organisation
    */
   function onAllOrgsListButtonPressed(org: OrganisationData) {
-    console.log("Adding org to active orgs: ", allOrganisations.filter((o) => o.org_id === org.org_id)[0].orgName);
+    console.log(
+      "Adding org to active orgs: ",
+      allOrganisations.filter((o) => o.org_id === org.org_id)[0].orgName
+    );
     if (!currentStudent) {
       return;
     }
-    updateCurrentStudent({activeOrgs: [...currentStudent.activeOrgs, org.org_id]});
+    updateCurrentStudent({ activeOrgs: [...currentStudent.activeOrgs, org.org_id] });
   }
 
-  const handleFocus = () => {
-    
-  };
+  function changeSortBy(value: string): void {
+      setSortBy(value);
+      let sortedOrgs = displayedOrganisations;
+      switch (value) {
+        case "name_asc": {
+          sortedOrgs.sort((a, b) => a.orgName.localeCompare(b.orgName));
+          break;
+        }
+  
+        case "name_desc": {
+          sortedOrgs.sort((a, b) => b.orgName.localeCompare(a.orgName));
+          break;
+        }
+  
+        case "distance_close": {
+          break;
+        }
+        case "distance_far": {
+          break;
+        }
+  
+        default:
+          break;
+      }
+  }
 
-const handleBlur = (searchInput: string) => {
+
+  const handleBlur = (searchInput: string) => {
     if (searchInput.trim() === "") {
       setDisplayedOrganisations(allOrganisations);
       return;
     }
-    const filteredOrgs = allOrganisations.filter((org) => org.orgName.toLowerCase().includes(searchInput.toLowerCase().trim()));
+
+    let filteredOrgs = allOrganisations.filter((org) =>
+      org.orgName.toLowerCase().includes(searchInput.toLowerCase().trim())
+    );
+    switch (sortBy) {
+      case "name_asc": {
+        filteredOrgs.sort((a, b) => a.orgName.localeCompare(b.orgName));
+        break;
+      }
+
+      case "name_desc": {
+        filteredOrgs.sort((a, b) => b.orgName.localeCompare(a.orgName));
+        break;
+      }
+
+      case "distance_close": {
+        break;
+      }
+      case "distance_far": {
+        break;
+      }
+
+      default:
+        break;
+    }
+
     if (filteredOrgs.length === 0) {
       Alert.alert("No organisations found", "Please try a different search term.");
       return;
     }
     setDisplayedOrganisations(filteredOrgs);
   };
-    
-  
 
   const handleRequestNewOrganisation = () => {
-      navigation.navigate("RequestOrgScreen" as never);
-    };
-  
-    const handleRequestProgress = () => {
-      navigation.navigate("RequestProgressScreen" as never);
-    };
-  
-    const handleRemoveOrganisation = () => {
-      navigation.navigate("RemoveOrgScreen" as never);
-    };
+    navigation.navigate("RequestOrgScreen" as never);
+  };
 
-// This constant is defined here to keep the render method clean and readable.
+  const handleRequestProgress = () => {
+    navigation.navigate("RequestProgressScreen" as never);
+  };
+
+  const handleRemoveOrganisation = () => {
+    navigation.navigate("RemoveOrgScreen" as never);
+  };
+
+  // This constant is defined here to keep the render method clean and readable.
   // By defining it outside of the return statement, we can perform any necessary
   // calculations or logic before rendering the JSX. This approach helps in:
   // 1. Avoiding clutter in the return statement.
   // 2. Making the code more modular and easier to maintain.
-  // 3. Allowing reuse of the constant if needed in multiple places within the component.  
+  // 3. Allowing reuse of the constant if needed in multiple places within the component.
   const renderContent = () => (
     <View style={styles.page}>
       <Text style={styles.pageHeading}>Your Organisations</Text>
-    
-      {studentOrganisations.length > 0 ? <ExpandableOrgList
-       items={studentOrganisations}
-       onListButtonClicked={onStudentOrgsListButtonPressed}
-       listButtonComp={
-         <CustomButton
-           onPress={() => {}}
-           title="Start Tracking"
-           textSize={18}
-           buttonColor="#A4DB51"
-           textColor="#000000"
-           fontFamily="Rany-Bold"
-         />
-       } 
-      />   : studentsOrgsLoaded ?  <Text> Select up to 4 organisations list below</Text> : <Text>Loading...</Text>
-      }
+
+      {studentOrganisations.length > 0 ? (
+        <ExpandableOrgList
+          items={studentOrganisations}
+          onListButtonClicked={onStudentOrgsListButtonPressed}
+          listButtonComp={
+            <CustomButton
+              onPress={() => {}}
+              title="Start Tracking"
+              textSize={18}
+              buttonColor="#A4DB51"
+              textColor="#000000"
+              fontFamily="Rany-Bold"
+            />
+          }
+        />
+      ) : studentsOrgsLoaded ? (
+        <Text> Select up to 4 organisations list below</Text>
+      ) : (
+        <Text>Loading...</Text>
+      )}
       <Text style={styles.sectionHeading}>Organisation Management</Text>
       <View style={styles.buttonWrapper}>
         <CustomButton
@@ -203,35 +259,33 @@ const handleBlur = (searchInput: string) => {
         />
       </View>
       <Text style={styles.sectionHeading}>Add Organisation</Text>
-  
+
       <View style={styles.inputRow}>
-      <View style={styles.inputWrapper}>
-      <SearchBarComponent
-      FGColor="#000000"
-      onSearchInputChange={(searchInput) => console.log(searchInput)}
-      placeHolderColor="#A9A9A9"
-      onFocus={handleFocus}
-      onBlur={handleBlur}
-      />
-  </View>
-  <View style={styles.inputWrapper}>
-      <ComboBoxComponent
-        options={[
-          { label: "Name (A-Z)", value: "name_asc" },
-          { label: "Name (Z-A)", value: "name_desc" },
-          { label: "Distance (Closest)", value: "distance_close" },
-          { label: "Distance (Farthest)", value: "distance_far" },
-        ]}
-        placeholder="Sort By"
-        FGColor="#969696"
-        placeHolderColor="#A9A9A9"
-        value="name_asc"
-        onValueChange={(value: string) => console.log(value)}
-      />
-      
-</View>
-</View>
-<ExpandableOrgList
+        <View style={styles.inputWrapper}>
+          <SearchBarComponent
+            FGColor="#000000"
+            onSearchInputChange={(searchInput) => console.log(searchInput)}
+            placeHolderColor="#A9A9A9"
+            onBlur={handleBlur}
+          />
+        </View>
+        <View style={styles.inputWrapper}>
+          <ComboBoxComponent
+            options={[
+              { label: "Name (A-Z)", value: "name_asc" },
+              { label: "Name (Z-A)", value: "name_desc" },
+              { label: "Distance (Closest)", value: "distance_close" },
+              { label: "Distance (Farthest)", value: "distance_far" },
+            ]}
+            placeholder="Sort By"
+            FGColor="#969696"
+            placeHolderColor="#A9A9A9"
+            value={sortBy}
+            onValueChange={(value: string) => changeSortBy(value)}
+          />
+        </View>
+      </View>
+      <ExpandableOrgList
         items={displayedOrganisations}
         onListButtonClicked={onAllOrgsListButtonPressed}
         listButtonComp={
@@ -245,69 +299,69 @@ const handleBlur = (searchInput: string) => {
           />
         }
       />
-</View>
-);
-
-if (loading) {
-  return <Text>Loading student data...</Text>;
-}
-
-if (error) {
-  return <Text>Error loading student data: {error.message}</Text>;
-}
-
-if (!currentStudent) {
-  return <Text>No student data available.</Text>;
-}
-  return (
-
-    <SafeAreaView style={styles.safeArea}>
-              <FlatList
-              data={itemList}
-              renderItem={null} // Use `renderItem` to handle FlatList rendering, but in this case we are rendering static content
-              ListHeaderComponent={renderContent} // This ensures scrollable content
-              keyExtractor={(item, index) => index.toString()}
-            />
-    </SafeAreaView>
+    </View>
   );
+
+  if (loading) {
+    return <Text>Loading student data...</Text>;
   }
 
-  
-  const styles = StyleSheet.create({
-    safeArea: {
-      flex: 1,
-      backgroundColor: "#F9F9F9",
-    },
-    page: {
-      backgroundColor: "#F9F9F9",
-      flex: 1,
-      padding: 16,
-    },
-    pageHeading: {
-      fontSize: 30,
-      fontFamily: "Quittance",
-      color: "#161616",
-      marginTop: 30,
-      marginBottom: 15,
-    },
-    sectionHeading: {
-      fontSize: 20,
-      fontFamily: "Quittance",
-      color: "#161616",
-      marginTop: 30,
-      marginBottom: 15,
-    },
-    buttonWrapper: {
-      marginBottom: 8, // Adjust the value for desired spacing between buttons
-    },
-    inputRow: {
-      flexDirection: 'row',
-      justifyContent: 'space-between',
-      alignItems: 'center',
-    },
-    inputWrapper: {
-      flex: 1,
-      marginHorizontal: 5, // Optional: Add some horizontal margin for spacing
-      marginBottom: 12, // Optional: Add some vertical margin for spacing
-    },
-  });
+  if (error) {
+    return <Text>Error loading student data: {error.message}</Text>;
+  }
+
+  if (!currentStudent) {
+    return <Text>No student data available.</Text>;
+  }
+  return (
+    <SafeAreaView style={styles.safeArea}>
+      <FlatList
+        data={itemList}
+        renderItem={null} // Use `renderItem` to handle FlatList rendering, but in this case we are rendering static content
+        ListHeaderComponent={renderContent} // This ensures scrollable content
+        keyExtractor={(item, index) => index.toString()}
+      />
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  safeArea: {
+    flex: 1,
+    backgroundColor: "#F9F9F9",
+  },
+  page: {
+    backgroundColor: "#F9F9F9",
+    flex: 1,
+    padding: 16,
+  },
+  pageHeading: {
+    fontSize: 30,
+    fontFamily: "Quittance",
+    color: "#161616",
+    marginTop: 30,
+    marginBottom: 15,
+  },
+  sectionHeading: {
+    fontSize: 20,
+    fontFamily: "Quittance",
+    color: "#161616",
+    marginTop: 30,
+    marginBottom: 15,
+  },
+  buttonWrapper: {
+    marginBottom: 8, // Adjust the value for desired spacing between buttons
+  },
+  inputRow: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  inputWrapper: {
+    flex: 1,
+    marginHorizontal: 5, // Optional: Add some horizontal margin for spacing
+    marginBottom: 12, // Optional: Add some vertical margin for spacing
+  },
+});
+
+
