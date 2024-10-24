0306c7499430ed6ebb0694e420ae2d828d00395f
Try-Angle1134 <Jaspervann1313@gmail.com>
2024-10-24 15:40:09 +0200

    Refactor imports in RegisterProfilePhotoScreen and RegisterScreen

--- src/screens/RegisterProfilePhotoScreen.tsx
+++ src/screens/RegisterProfilePhotoScreen.tsx
@@ -1,4 +1,4 @@
-import { Alert, StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 import AvatarComponent from "../components/AvatarComponent";
 import CustomButton from "../components/CustomButton";
 import { useState } from "react";
