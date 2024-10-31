d80b580bc63779e2c22c96c7265a7427ae58313c
Nicholas Meyer <ST10204902@vcconnect.edu.za>
2024-10-31 18:39:28 +0200

    Update StudentHeaderComponent.tsx

--- src/components/StudentHeaderComponent.tsx
+++ src/components/StudentHeaderComponent.tsx
@@ -2,11 +2,10 @@ import React from "react";
 import { View, Text, StyleSheet, Image } from "react-native";
 import { LinearGradient } from "expo-linear-gradient"; // Import the expo gradient
 import { Student } from "../databaseModels/databaseClasses/Student";
-import { useTheme } from '../styles/ThemeContext';
-import { lightTheme, darkTheme } from '../styles/themes'; // Import themes
-import MyMaths from '../utils/MyMaths';
-
- 
+import { useTheme } from "../styles/ThemeContext";
+import { lightTheme, darkTheme } from "../styles/themes"; // Import themes
+import { AvatarUtility } from "../utils/AvatarUtility";
+import MyMaths from "../utils/MyMaths";
 
 interface StudentHeaderComponentProps {
   currentStudent: Student;
@@ -18,26 +17,30 @@ const StudentHeaderComponent: React.FC<StudentHeaderComponentProps> = ({
   const { isDarkMode } = useTheme();
   const theme = isDarkMode ? darkTheme : lightTheme;
 
+  const avatarSource = AvatarUtility.getAvatarSource(currentStudent.profilePhotoId || "avatar_1");
+
   const completedHours = MyMaths.calculateTotalLoggedHours(currentStudent);
 
-  
-  const progress =  (completedHours / 4) * 100 ;
+  const progress = (completedHours / 4) * 100;
 
-  const completedHoursRounded  = (Math.round(completedHours * 100) / 100) >= 4 ? 4 : (Math.round(completedHours * 100) / 100);
+  const completedHoursRounded =
+    Math.round(completedHours * 100) / 100 >= 4
+      ? 4
+      : Math.round(completedHours * 100) / 100;
 
   const code = currentStudent.studentNumber;
 
   // yes, this looks disgusting, but it works and that's what matters
   const possibleAvatars = [
-    require("../assets/Avatars/A9.png"), 
-     require("../assets/Avatars/A8.png"),
-     require("../assets/Avatars/A7.png"),
-     require("../assets/Avatars/A6.png"),
-     require("../assets/Avatars/A5.png"),
-     require("../assets/Avatars/A4.png"),
-     require("../assets/Avatars/A3.png"),
-     require("../assets/Avatars/A2.png"),
-     require("../assets/Avatars/A1.png"),
+    require("../assets/Avatars/A9.png"),
+    require("../assets/Avatars/A8.png"),
+    require("../assets/Avatars/A7.png"),
+    require("../assets/Avatars/A6.png"),
+    require("../assets/Avatars/A5.png"),
+    require("../assets/Avatars/A4.png"),
+    require("../assets/Avatars/A3.png"),
+    require("../assets/Avatars/A2.png"),
+    require("../assets/Avatars/A1.png"),
   ];
   const valuePairs = {
     "../assets/Avatars/A9.png": 0,
@@ -53,11 +56,14 @@ const StudentHeaderComponent: React.FC<StudentHeaderComponentProps> = ({
 
   // set default avatar
   let userAvatar = require("../assets/Avatars/A1.png");
-  
+
   // check if student has a profile
-  if (currentStudent.profilePhotoURL != "" && currentStudent.profilePhotoURL != null) {
+  if (
+    currentStudent.profilePhotoId != "" &&
+    currentStudent.profilePhotoId != null
+  ) {
     // get the index of the avatar
-    let pfpURL = (currentStudent.profilePhotoURL) as keyof typeof valuePairs;
+    let pfpURL = currentStudent.profilePhotoId as keyof typeof valuePairs;
     let index = valuePairs[pfpURL];
     // set the avatar
     userAvatar = possibleAvatars[index];
@@ -65,11 +71,18 @@ const StudentHeaderComponent: React.FC<StudentHeaderComponentProps> = ({
 
   return (
     <View style={styles.container1}>
-      <Image source={userAvatar} style={styles.emoji} />
+      <Image source={avatarSource} style={styles.emoji} />
       <View style={styles.container2}>
         <Text style={[styles.code, { color: theme.fontRegular }]}>{code}</Text>
         <Text
-          style={[styles.progressText, { color: theme.fontRegular, textAlign: 'left', alignSelf: 'flex-start' }]}
+          style={[
+            styles.progressText,
+            {
+              color: theme.fontRegular,
+              textAlign: "left",
+              alignSelf: "flex-start",
+            },
+          ]}
         >{`${completedHoursRounded} out of 4 hours completed`}</Text>
         <View style={styles.progressBar}>
           <View style={styles.progressBar}>
@@ -100,7 +113,7 @@ const styles = StyleSheet.create({
   },
   studentNumber: {
     fontSize: 14,
-    color: '#666666',
+    color: "#666666",
     marginBottom: 4,
   },
   emoji: {
@@ -114,8 +127,8 @@ const styles = StyleSheet.create({
   },
   progressText: {
     fontSize: 16,
-    textAlign: 'left', // Align text to the start
-    alignSelf: 'flex-start', // Align text container to the start
+    textAlign: "left", // Align text to the start
+    alignSelf: "flex-start", // Align text container to the start
     marginTop: 2,
     fontFamily: "Rany-Regular",
     marginBottom: 3,
