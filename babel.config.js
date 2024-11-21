5545e7f03f525f2065be56cd31e370607cef7d1d
Nicholas Meyer <ST10204902@vcconnect.edu.za>
2024-11-21 13:51:31 +0200

    Fixed babel config issue

--- babel.config.js
+++ babel.config.js
@@ -1,14 +1,9 @@
-module.exports = {
-  presets: [
-    ["@babel/preset-env", { targets: { node: "current" } }],
-    "@babel/preset-typescript",
-    ["@babel/preset-react", { runtime: "automatic" }],
-    "module:metro-react-native-babel-preset",
-  ],
-  plugins: [
-    "react-native-reanimated/plugin",
-    "@babel/plugin-transform-private-methods",
-    "@babel/plugin-transform-class-properties",
-    "@babel/plugin-transform-private-property-in-object",
-  ],
+module.exports = function (api) {
+  api.cache(true);
+  return {
+    presets: ['babel-preset-expo'],
+    plugins: [
+      'react-native-reanimated/plugin',
+    ],
+  };
 };
